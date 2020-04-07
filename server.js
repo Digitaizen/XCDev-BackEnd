74d1d5456afc95ed8af1233f03eb5aa611e677ca
Travis Teeter <Travis_Teeter@dell.com>
2020-04-07 14:54:50 -0700

    This commit will update the MongoDB servers collection with JSON data from the v1 and Systems URIs

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-/**
- * PSeudo Code
- * IPAddresses = [IPAddresses1, IPAddresses2, ]
-        
-    const systemInventory = {}
-                {
-                fetchCall -> data
-                    systemIventory.servicetag = data.Oem.Dell.Service
-                secondFetchCall -> data
-                    systemInventory.model = data.System.Model
-                console.log(systemInventory);           
-                }
- 
-        send(systemInventory) -> toMongoDB
-        MongoDBUI -> object 
- */
-
 "use strict";
 
 console.log("Server side code running");
@@ -30,7 +13,7 @@ const app = express();
 // Defining the directory where express will serve the website
 app.use(express.static("public"));
 
-// Local mongodb database session
+// Local monogodb database session
 const url = "mongodb://localhost:27017";
 let db;
 
@@ -45,80 +28,104 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
 });
 
 // Default reply for home page
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
 // Make public API call to jsonPlaceholder for users and save the response data in database
-app.post("/postServers", (req, res) => {
-  //   db.collection("users").deleteMany({});
-  // });
 
-  // List of hard-coded iDRAC IPs for testing
-  let idracIps = ["100.80.146.94", "100.80.146.97"];
+//   db.collection("users").deleteMany({});
+// });
 
-  // Iterate through iDRAC IPs
-  idracIps.forEach(function(item, index) {
-    // Define the inputs for a fetch call
-    let idracUrl = "https://" + item + "/redfish/v1";
-    let login = "root";
-    let password = "calvin";
-    const agent = new https.Agent({
-      rejectUnauthorized: false
-    });
+// List of hard-coded iDRAC IPs for testing
+let idracIps = ["100.80.146.100", "100.80.146.97"];
 
-    let options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${base64.encode(`${login}:${password}`)}`
-      },
-      agent: agent
-    };
+// Iterate through iDRAC IPs
+idracIps.forEach(function(item, index) {
+  let emptyObject = {};
+  // Define the inputs for a fetch call
+  let idracUrl = "https://" + item + "/redfish/v1";
+  let anotherUrl = "https://" + item + "/redfish/v1/Systems/System.Embedded.1";
 
-    // Make fetch call
-    fetch(idracUrl, options, {
-      method: "GET"
-    })
-      .then(function(response) {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(function(data) {
-        // Check collection for identical entry
-        db.collection("servers").findOne({ ip: item }, (err, results) => {
-          if (err) {
-            return console.log(err);
-          }
-          // Update old collection entry if a match is found
-          if (results !== null) {
-            db.collection("servers").updateOne(
-              { ip: item },
-              { $set: { ip: item, redfishVersion: data.RedfishVersion } },
-              (err, res) => {
-                if (err) {
-                  return console.log(err);
-                }
-                console.log("Server updated in db");
-              }
-            );
-            // Add new collection entry if no match is found
-          } else {
-            db.collection("servers").insertOne(
-              { ip: item, redfishVersion: data.RedfishVersion },
-              (err, res) => {
-                if (err) {
-                  return console.log(err);
-                }
-                console.log("Server added to db");
-              }
-            );
-          }
-        });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+  let key1 = "v1";
+  let key2 = "System";
+
+  let login = "root";
+  let password = "calvin";
+  const agent = new https.Agent({
+    rejectUnauthorized: false
   });
+
+  let options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${base64.encode(`${login}:${password}`)}`
+    },
+    agent: agent
+  };
+
+  // Make fetch call
+  fetch(idracUrl, options, {
+    method: "GET"
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(function(v1Data) {
+      emptyObject[key1] = v1Data;
+
+      return fetch(anotherUrl, options, {
+        method: "GET"
+      });
+    })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(function(systemData) {
+      emptyObject[key2] = systemData;
+
+      return db.collection("servers").findOne({ ip: item }, (err, results) => {
+        if (err) {
+          return console.log(err);
+        }
+        // Update old collection entry if a match is found
+        if (results !== null) {
+          db.collection("servers").updateOne(
+            { ip: item },
+            { $set: { ip: item, data: emptyObject } },
+            (err, res) => {
+              if (err) {
+                return console.log(err);
+              }
+              console.log("Server updated in db");
+            }
+          );
+          // Add new collection entry if no match is found
+        } else {
+          db.collection("servers").insertOne(
+            { ip: item, data: emptyObject },
+            (err, res) => {
+              if (err) {
+                return console.log(err);
+              }
+              console.log("Server added to db");
+            }
+          );
+        }
+        // console.log("v1 object: ", emptyObject.v1);
+        // console.log("System object: ", emptyObject.System);
+      });
+    })
+    .catch(function(error) {
+      console.warn(error);
+    });
 });
