9dd14aef3a54d308bb5e4a8f658f43d7a4a50701
Travis Teeter <Travis_Teeter@dell.com>
2020-06-15 12:44:18 -0700

    This commit will make "message" the prop name for all error responses DELLXCDEV-80

--- server.js
+++ server.js
@@ -230,12 +230,12 @@ function validateLoginInput(data) {
 
   // Username checks
   if (Validator.isEmpty(data.username)) {
-    errors.email = "Username field is required";
+    errors.message = "Username field is required";
   }
 
   // Password checks
   if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
+    errors.message = "Password field is required";
   }
 
   return {
@@ -257,41 +257,32 @@ function validateRegisterInput(data) {
   data.email = !isEmpty(data.email) ? data.email : "";
   data.username = !isEmpty(data.username) ? data.username : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  // data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
   // Name checks
   if (Validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
+    errors.message = "Name field is required";
   }
 
   // Email checks
   if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
+    errors.message = "Email field is required";
   } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+    errors.message = "Email is invalid";
   }
 
   // Username checks
   if (Validator.isEmpty(data.username)) {
-    errors.name = "Username field is required";
+    errors.message = "Username field is required";
   }
 
   // Password checks
   if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
+    errors.message = "Password field is required";
   }
-
-  // if (Validator.isEmpty(data.password2)) {
-  //   errors.password2 = "Confirm password field is required";
-  // }
 
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be at least 6 characters";
+    errors.message = "Password must be at least 6 characters";
   }
-
-  // if (!Validator.equals(data.password, data.password2)) {
-  //   errors.password2 = "Passwords must match";
-  // }
 
   return {
     errors,
@@ -375,9 +366,7 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true, poolSize: 10 }).then(
         .then(user => {
           // Check if user exists
           if (!user) {
-            return res
-              .status(404)
-              .json({ emailnotfound: "Username not found" });
+            return res.status(404).json({ message: "Username not found" });
           }
 
           // Check password
@@ -406,9 +395,7 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true, poolSize: 10 }).then(
               }
             );
           } else {
-            return res
-              .status(400)
-              .json({ passwordincorrect: "Password incorrect" });
+            return res.status(400).json({ message: "Password incorrect" });
           }
         });
     });
@@ -430,7 +417,7 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true, poolSize: 10 }).then(
           .findOne({ email: req.body.email })
           .then(user => {
             if (user) {
-              return res.status(400).json({ email: "Email already exists" });
+              return res.status(400).json({ message: "Email already exists" });
             } else {
               _db
                 .collection(dbColl_Users)
@@ -528,21 +515,19 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true, poolSize: 10 }).then(
         .then(user => {
           // Check if user exists
           if (!user) {
-            return res
-              .status(404)
-              .json({ emailnotfound: "Username not found" });
+            return res.status(404).json({ message: "Username not found" });
           }
 
           // Check if password is long enough
           if (!Validator.isLength(req.body.password, { min: 6, max: 30 })) {
             return res
               .status(404)
-              .json({ password: "Password must be at least 6 characters" });
+              .json({ message: "Password must be at least 6 characters" });
           }
 
           // Check if passwords match
           if (!Validator.equals(req.body.password, req.body.password2)) {
-            return res.status(404).json({ password2: "Passwords must match" });
+            return res.status(404).json({ message: "Passwords must match" });
           }
 
           // Update user record with new password
