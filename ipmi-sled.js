c842b286ee9a3c2e8092a52705ba71ed619355c7
Azat <Azat_Salikhov@Dellteam.com>
2020-07-23 13:35:22 -0500

    WIP - This commit is in ref to DELLXCDEV-110. Created module to pull server blade slot position via direct IPMI call for raw data.

---
+++ ipmi-sled.js
@@ -0,0 +1,60 @@
+// Module to get a server's sled/blade slot position 
+// via IPMI call for raw data
+const { exec } = require('child_process');
+
+export default function getSlotPosition(ip) {
+    const errMsg = "NOT_SUPPORTED";
+    const userName = "root";
+    const userPass = "calvin";
+    let sysType = "";
+    let sysSled = "";
+
+    try {
+        exec(`ipmitool  -I lanplus -H ${ip} -U ${userName} -P ${userPass} raw  0x06 0x59 0x00 0xDD 0x02 0x00`, (err, stdout, stderr) => {
+            if (err || stderr) {
+                // Debugging, some error occurred
+                console.error(err);
+                console.log(`stderr: ${stderr}`);
+
+                return errMsg;
+            } else if (stdout) {
+                // Debugging, the *entire* stdout (buffered)
+                console.log(`stdout: ${stdout}`);
+
+                // Parse the raw output and get the 11th byte that indicates the system type
+                sysType = stdout.split(' ')[10];
+
+                // Execute further query if the system has mutliple blades
+                if (sysType.substr(1, 1) === "2") {
+                    exec(`ipmitool  -I lanplus -H ${ip} -U ${userName} -P ${userPass} raw 0x30 0x12`, (err, stdout, stderr) => {
+                        if (err || stderr) {
+                            // Debugging, some error occurred
+                            console.error(err);
+                            console.log(`stderr: ${stderr}`);
+
+                            return errMsg;
+                        } else {
+                            // Debugging, the *entire* stdout (buffered)
+                            console.log(`stdout: ${stdout}`);
+
+                            // Get the 10th byte that has the slot position info
+                            sysSled = stdout.split(' ')[9];
+
+                            return sysSled;
+                        }
+                    });
+                } else {
+                    // Debugging 
+                    console.log('This system sled info is not supported');
+
+                    return errMsg;
+                }
+            }
+        });
+    } catch (err) {
+        // Debugging 
+        console.log("Error: ", err);
+
+        return "error";
+    }
+}
--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 const cors = require("cors");
 const morganBody = require("morgan-body");
+const iDracSled = require("./ipmi-sled");
 
 // Declare the globals ////////////////////////////////////////////////////////
 const dbUrl = "mongodb://localhost:27017";
@@ -632,7 +633,7 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true, poolSize: 10 }).then(
                 password: req.body.password
               }
             },
-            function(err, results) {
+            function (err, results) {
               if (err) {
                 res.status(500).json(Object.assign({ success: false }, err));
               } else {
