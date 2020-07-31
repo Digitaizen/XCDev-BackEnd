f8541ceebfd95123f823a9adb7cdc84aa7b9ed5c
Azat <Azat_Salikhov@Dellteam.com>
2020-07-30 23:06:36 -0500

    This commit is in ref to DELLXCDEV-110. Fixed the code and live-tested it.

--- ipmi-sled.js
+++ ipmi-sled.js
@@ -2,44 +2,49 @@
 // via IPMI call for raw data
 const { exec } = require('child_process');
 
-export default function getSlotPosition(ip) {
+function getSlotPosition(ip) {
     const userName = "root";
     const userPass = "calvin";
-    const serverType = new Set(["22", "32", "42"]);
+    const serverType = new Set([22, 32, 42]);
     const errMsg = "NOT_SUPPORTED";
     let sysType = "";
     let sysSled = "";
 
     try {
+        console.log(`Quering iDRAC @${ip} via IPMI call for system type..`);
         exec(`ipmitool  -I lanplus -H ${ip} -U ${userName} -P ${userPass} raw  0x06 0x59 0x00 0xDD 0x02 0x00`, (err, stdout, stderr) => {
             if (err || stderr) {
                 // Debugging, some error occurred
                 console.error(err);
-                console.log(`stderr: ${stderr}`);
+                console.log(`1st query, stderr: ${stderr}`);
 
                 return errMsg;
             } else if (stdout) {
                 // Debugging, the *entire* stdout (buffered)
-                console.log(`stdout: ${stdout}`);
+                console.log(`1st query, stdout, raw string: ${stdout}`);
 
                 // Parse the raw output and get the 11th byte that indicates the system type
-                sysType = stdout.split(' ')[10];
+                sysType = parseInt(stdout.split(' ')[11]);
 
-                // Execute further query if the system has mutliple blades
+                console.log("System Type num is: ", sysType);
+
+                // Execute further query if the system has mutliple blades		
                 if (serverType.has(sysType)) {
+                    console.log("Now quering for its blade slot position..");
                     exec(`ipmitool  -I lanplus -H ${ip} -U ${userName} -P ${userPass} raw 0x30 0x12`, (err, stdout, stderr) => {
                         if (err || stderr) {
                             // Debugging, some error occurred
                             console.error(err);
-                            console.log(`stderr: ${stderr}`);
+                            console.log(`2nd query, stderr: ${stderr}`);
 
                             return errMsg;
                         } else {
                             // Debugging, the *entire* stdout (buffered)
-                            console.log(`stdout: ${stdout}`);
+                            console.log(`2nd query, stdout, raw string: ${stdout}`);
 
                             // Get the 10th byte that has the slot position info
-                            sysSled = stdout.split(' ')[9];
+                            sysSled = stdout.split(' ')[10];
+                            console.log(`Blade Slot num is: ${sysSled}`);
 
                             return sysSled;
                         }
